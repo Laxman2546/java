817f54e962235585d6a9de5600948715a90decb2
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-10-18 16:52:10 +0530

    character and word count added

--- WordCount.java
+++ WordCount.java
@@ -4,17 +4,25 @@ class WordCount {
     public static int counter(String word) {
         int count = 0;
         for (int i = 0; i < word.length(); i++) {
-            word.charAt(i);
-            count++;
+            if (word.charAt(i) != ' ') {
+                count++;
+            }
         }
         return count;
     }
+
+    public static int noofWords(String Words) {
+        String[] word = Words.trim().split("\\s+");
+        return word.length;
+    } 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Your word: ");
-        String word = scanner.next();
-        int noWords = counter(word);
-        System.out.println(noWords);
+        System.out.print("Enter Your word:");
+        String word = scanner.nextLine();
+        int noCharcters = counter(word);
+        int noWords = noofWords(word);
+        System.out.println("Character Count:"+ noCharcters);
+        System.out.println("Words Count:"+ noWords);
     }
 }
 
