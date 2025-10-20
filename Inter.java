de215b2667cac32f13f0d0f1e3ab5ca1ca2edcf7
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-10-20 18:15:51 +0530

    changes

--- Inter.java
+++ Inter.java
@@ -3,15 +3,44 @@ interface user {
 }
 
 class userName implements user {
-       public void name() {
+    public void name() {
         System.out.println("iam name");
     }
 }
 
+abstract class shape {
+    String color;
+
+    shape(String color) {
+        this.color = color;
+        System.out.println(color);
+    }
+
+    public abstract int length();
+}
+
+class circle extends shape {
+    private int radius;
+    
+    public circle(String color,int radius) {
+        super(color);
+        this.radius = radius;
+        calcRadius(radius);
+    }
+    public int calcRadius(int radius) {
+        return length();
+    }
+    @Override
+    public int length() {
+        return  (int)Math.PI * radius;        
+    }
+}
 
 class Inter{
    public static void main(String[] args){
-       userName name = new userName();
-       name.name();
+       circle cr = new circle("blue",12);
+
+       int len = cr.calcRadius(24);
+       System.out.println(len);
    }    
 }
